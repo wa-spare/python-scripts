a6c6980cf1339c0bcbe48df1cd84a7d1b5dae9c0
brave <wa_spare@hotmail.com>
2022-08-05 13:14:49 -0400

    Create list-hosts.py
    Using v1 host endpoint

---
+++ list-hosts/list-hosts.py
@@ -0,0 +1,31 @@
+import requests,json
+
+tenant = 'https://{tenant}.live.dynatrace.com'
+key = '{key}'
+
+url = "/api/v1/entity/infrastructure/hosts?includeDetails=false"
+outputFile = open('host_list.csv', 'w')
+outputFile.write('hostname; managementzone; hostunits' + "\n")
+page = 1
+npk=''
+npk_list =[]
+print('Printing all Hosts to host_list.csv.  Please wait...')
+
+req_url = f'{tenant}{url}?format=json&api-token={key}' 
+print(req_url)
+response = requests.get(req_url)
+jresponse = response.json()
+
+#print(jresponse1)
+for record in jresponse:
+    try: 
+        host1 = record['displayName']
+        host2 = record['discoveredName']
+        hus = record['consumedHostUnits']
+        mzs = record['managementZones'][0]['name']
+    except:
+        host1 = record['displayName']
+        host2 = record['discoveredName']
+        hus = record['consumedHostUnits']
+        mzs = 'No MZ assigned'
+    outputFile.write(f'{host2};{mzs};{hus}' + "\n")
